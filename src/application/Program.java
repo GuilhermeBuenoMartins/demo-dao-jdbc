3db2bb85195c80131d735e06fb49c42695af9c6f
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2021-03-29 11:40:06 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,9 @@
 package application;
 
-import entities.Department;
-import entities.Seller;
-
-import java.util.Date;
-
 public class Program {
 
     public static void main(String[] args) {
+
 
     }
 }
