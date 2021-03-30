5efbddddfcc4c1347d485c2cee8c06acfb00cfa1
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2021-03-30 11:41:27 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,7 @@
 package application;
 
-import model.dao.DB;
-import model.dao.DaoFactory;
-import model.dao.SellerDao;
-
-import java.sql.Connection;
-
 public class Program {
 
     public static void main(String[] args) {
-        
     }
 }
